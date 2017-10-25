3d6e714ad4033998ef28d7a3c20eae30e3e1811c
JosephDGarza <32659326+JosephDGarza@users.noreply.github.com>
2017-10-24 23:14:31 -0400

    correction to what was uploaded

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,15 +1,33 @@
 import java.util.Scanner;
 
 public class Lab1 {
-	   public static void main (String [] args) {
-		      Scanner scnr = new Scanner(System.in);
-		      
-		      System.out.println("Please enter a number:");
-		      
-		      int userNumber = 0;
-		      
-		      userNumber = scnr.nextInt();
-		      
-		      System.out.println(userNumber + 1);
+public static void main(String[] args) {
+	Scanner scnr = new Scanner(System.in);
+	double length = 0;
+	double width = 0;
+	double area = 0;
+	double perimeter = 0;
+	String userInput = "y";
+	
+	do {
+	System.out.print("Enter Length: ");
+	length = scnr.nextDouble();
+	System.out.print("Enter Width: ");
+	width = scnr.nextDouble();
+	
+	
+	area = length * width;
+	perimeter = length * 2 + width * 2;
+	
+	System.out.println("Area: " + area);
+	System.out.println("Perimeter: " + perimeter);
+	
+	System.out.println("Continue? (y/n)");
+	userInput = scnr.next();
+	
+	}
+	while (userInput.equals("y")); {
+}
+	System.out.println("Bye.");
 }
 }
